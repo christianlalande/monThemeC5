c1b86bbba2e82cd5e94bf26dd0ebe77dd7160a57
Christian Lalande <kistran@gmail.com>
2015-04-03 20:43:33 +0200

    Ajout de liens cliquables dans le footer

--- elements/footer.php
+++ elements/footer.php
@@ -4,11 +4,11 @@
             <div class="w-6">
                 <p>Site Powered by Concrete5. Design and Developed by OAM.</p>
                 <ul>
-                    <li><a href="http://localhost:8888/concrete5.6.3.2/index.php/products">Home</a></li>
-                    <li><a href="Products">Products</a></li> 
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="http://lalande.lprsc.crabdance.com">Home</a></li>
+                    <li><a href="http://lalande.lprsc.crabdance.com/index.php/products/">Products</a></li> 
+                    <li><a href="http://lalande.lprsc.crabdance.com/index.php/blog/">Blog</a></li>
+                    <li><a href="http://lalande.lprsc.crabdance.com/index.php/contacts/">Contacts</a></li>
+                    <li><a href="http://lalande.lprsc.crabdance.com/index.php/plan/">Plan</a></li>
                 </ul>
                 <ul>
                     <li><a href="#">Privacy Policy</a></li>
